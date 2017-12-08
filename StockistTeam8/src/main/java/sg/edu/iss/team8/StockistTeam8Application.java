dc3f45eb96c47dc1dabb0c16710c03c1f7645d1e
ASUS <ASUS@172.17.248.196>
2017-12-08 13:31:23 +0800

    project created

---
+++ StockistTeam8/src/main/java/sg/edu/iss/team8/StockistTeam8Application.java
@@ -0,0 +1,12 @@
+package sg.edu.iss.team8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockistTeam8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockistTeam8Application.class, args);
+	}
+}
---
+++ StockistTeam8/src/main/java/sg/edu/iss/team8/repository/UserRepository.java
@@ -0,0 +1,5 @@
+package sg.edu.iss.team8.repository;
+
+public interface UserRepository {
+
+}
