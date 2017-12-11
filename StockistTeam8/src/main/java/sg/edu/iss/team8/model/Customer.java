7fcd7b12ae06bfc6601cc14e4b51e3c3b38503c0
TOBMM <TOBMM@172.17.252.61>
2017-12-11 15:13:18 +0800

    rem ServletInitializer, create simple cust
    create simple cust

--- StockistTeam8/src/main/java/sg/edu/iss/team8/model/Customer.java
+++ StockistTeam8/src/main/java/sg/edu/iss/team8/model/Customer.java
@@ -1,5 +1,74 @@
 package sg.edu.iss.team8.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "customer")
 public class Customer {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "customerid")
+	private Integer customerId;
+	@Column(name = "customername")
+	private String customerName;
+	@Column(name = "address")
+	private String address;
+	@Column(name = "country")
+	private String country;
+	@Column(name = "contactnumber")
+	private String contactNumber;
+	
+	
+	public Customer() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Customer(Integer customerId, String customerName, String address, String country, String contactNumber) {
+		super();
+		this.customerId = customerId;
+		this.customerName = customerName;
+		this.address = address;
+		this.country = country;
+		this.contactNumber = contactNumber;
+	}
+
+	public Integer getCustomerId() {
+		return customerId;
+	}
+	public void setCustomerId(Integer customerId) {
+		this.customerId = customerId;
+	}
+	public String getCustomerName() {
+		return customerName;
+	}
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public String getContactNumber() {
+		return contactNumber;
+	}
+	public void setContactNumber(String contactNumber) {
+		this.contactNumber = contactNumber;
+	}
+	
 
 }
