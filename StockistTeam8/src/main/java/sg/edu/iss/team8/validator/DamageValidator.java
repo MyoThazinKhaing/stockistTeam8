f1abe189b4aa444d200d94040fd2576e85a4b7aa
natha <nathalie.nhu.y@gmail.com>
2017-12-12 21:23:00 +0800

    f1

--- StockistTeam8/src/main/java/sg/edu/iss/team8/validator/DamageValidator.java
+++ StockistTeam8/src/main/java/sg/edu/iss/team8/validator/DamageValidator.java
@@ -5,8 +5,11 @@ import org.springframework.validation.Errors;
 import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
 
-
 import sg.edu.iss.team8.model.Damage;
+import sg.edu.iss.team8.service.DamageService;
+import sg.edu.iss.team8.service.DamageServiceImpl;
+import sg.edu.iss.team8.service.ProductServiceImpl;
+
 
 @Component
 public class DamageValidator implements Validator {
@@ -18,17 +21,22 @@ public class DamageValidator implements Validator {
 
 	@Override
 	public void validate(Object arg0, Errors arg1) {
-		Damage damage=(Damage) arg0;	
-		if ((damage.getSendDate()!=null && damage.getReceiveDate()!=null)&&(damage.getSendDate().compareTo(damage.getReceiveDate()) > 0)) {
+		Damage damage = (Damage) arg0;
+		if ((damage.getSendDate() != null && damage.getReceiveDate() != null)
+				&& (damage.getSendDate().compareTo(damage.getReceiveDate()) > 0)) {
 			arg1.reject("receiveDate", "Receive date should be greater than send date.");
 			arg1.rejectValue("receiveDate", "error.dates", "receive date cannot prior than send date");
-	
+
 		}
-		
-		ValidationUtils.rejectIfEmptyOrWhitespace(arg1,"partNumber", "error.partNumber", "Part Number is required.");
-		ValidationUtils.rejectIfEmptyOrWhitespace(arg1, "quantity","error.quantity", " ");
+		ProductService dService = new ProductServiceImpl();
+		if((damage.getQuantity()>damage.getPartNumber()))
+
+		ValidationUtils.rejectIfEmptyOrWhitespace(arg1, "partNumber", "error.partNumber", "Part Number is required.");
+		ValidationUtils.rejectIfEmptyOrWhitespace(arg1, "quantity", "error.quantity", " ");
 		ValidationUtils.rejectIfEmptyOrWhitespace(arg1, "sendDate", "error.sendDate", "Please select the send date.");
 		
+		
+
 	}
 
 }
